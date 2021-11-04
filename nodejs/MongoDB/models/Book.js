54e5c8b211193f635590242c11be9bd4670b8823
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-11-04 19:04:20 +0300

    Express | Added Schema property type,default,required,unique and its descriptions.

--- nodejs/MongoDB/models/Book.js
+++ nodejs/MongoDB/models/Book.js
@@ -2,8 +2,23 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+/*
+        -type  =>  cotes: Number demek gibi bir şey. diğer typeler ile kullanıldığında önemli.
+        -default => default olarak bi değer girilsin mi? Birisi ezmedikçe o kalsın mı?
+        -required => bu alan gerekli!
+        -unique => bir başkası bu alana atanmış değeri kullanamasın.
+
+        Örnekleri aşağıda =>
+*/
+
 const BookSchema = new Schema({
-    title: String,
+    // title: String,  //alttaki gibi de yazılabilir eğer üsttekileri (type required..) kullanmak istersen.
+    title: {
+        type: String,
+        default: 'Eğer biri title girmezse title bu olsun',
+        required: true,
+        unique: true,
+    },
     comments: [{ message: String }],
     meta: {
         votes: Number,
