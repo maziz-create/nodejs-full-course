41ee68d556a8a9b176cb0902efda913732ec01bb
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-11-04 18:14:41 +0300

    Express | Added mongodb connection in 2 different ways

--- nodejs/MongoDB/app.js
+++ nodejs/MongoDB/app.js
@@ -3,11 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+mongoose.connect('mongodb://localhost/FirstRobo')
+  .then(() => console.log("MongoDB bağlantısı başarılı!"))
+  .catch((err) => console.log("MongoDB bağlantı hatası! ", err));
+/*
+farklı bir then catch yapısı =>
+mongoose.connect('mongodb://localhost/FirstRobo'); //bağlantı şekli üsttekiyle aynı.
+mongoose.connection.on('open', () => console.log("MongoDB bağlantısı başarılı!")); //eventEmitter kullanıyoruz işte.
+mongoose.connection.on('error', (err) => console.log("MongoDB bağlantı hatası! ", err));
+*/
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,12 +34,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
